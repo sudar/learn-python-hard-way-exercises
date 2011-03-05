cd24830b1b042bc5de4d97fd2a68da0c74ac2e0d
Sudar <sudar@sudarmuthu.com>
2011-03-05 20:16:30 +0530

    Eleventh exercise

---
+++ ex11/ex11_extra3.py
@@ -0,0 +1,8 @@
+print "How old are you, 5 years ago? ",
+age = raw_input()
+print "How tall are you? 5 years ago",
+height = raw_input()
+print "How much do you weigh 5 years ago?",
+weight = raw_input()
+
+print "So, you're %r old, %r tall and %r heavy, 5 years ago." % (age, height, weight)
