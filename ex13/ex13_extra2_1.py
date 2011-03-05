74db25f82314d7c9998bb9b10029259c2f2aceb0
Sudar <sudar@sudarmuthu.com>
2011-03-05 20:29:54 +0530

    13th exercise

---
+++ ex13/ex13_extra2_1.py
@@ -0,0 +1,6 @@
+from sys import argv
+
+script, first = argv
+
+print "The script is called:", script
+print "Your frist variable is:", first
---
+++ ex13/ex13_extra3.py
@@ -0,0 +1,13 @@
+from sys import argv
+
+script, first, second, third = argv
+
+print "The script is called:", script
+print "Your frist variable is:", first
+print "Your second variable is:", second
+print "Your third variable is", third
+
+fourth = raw_input("Enter fourth varibale: ")
+
+print "Your fourth variable is", fourth
+
